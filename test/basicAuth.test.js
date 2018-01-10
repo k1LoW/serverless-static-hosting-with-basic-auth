03f31d4427af9614d0a04b50b4338ae391b53f31
k1LoW <k1lowxb@gmail.com>
2018-01-10 11:13:30 +0900

    bonsai

--- test/basicAuth.test.js
+++ test/basicAuth.test.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const { basicAuth } = require(path.join(__dirname, '..', 'handler.js'));
 
-test.cb('handler.basicAuth check user/pass', (t) => {
+test.cb('handler.basicAuth pass valid user/pass', (t) => {
     const event = {
         Records: [
             {
@@ -23,6 +23,37 @@ test.cb('handler.basicAuth check user/pass', (t) => {
             headers: {
                 authorization: [{ key: 'Authorization', value: 'Basic dXNlcjpwYXNz' }]
             }
+        });
+        t.end();
+    };
+
+    basicAuth(event, context, callback);
+});
+
+test.cb('handler.basicAuth fail invalid user/pass', (t) => {
+    const event = {
+        Records: [
+            {
+                cf: {
+                    request: {
+                        headers: {
+                            authorization: [{ key: 'Authorization', value: 'Basic invalid' }]
+                        }
+                    }
+                }
+            }
+        ]
+    };
+    const context = {};
+    const callback = (error, response) => {
+        t.deepEqual(response, {
+            status: '401',
+            statusDescription: 'Authorization Required',
+            headers: {
+                'www-authenticate': [{ key: 'WWW-Authenticate', value: 'Basic' }],
+                'content-type': [{ key: 'Content-Type', value: 'text/plain; charset=utf-8' }]
+            },
+            body: '401 Authorization Required'
         });
         t.end();
     };
